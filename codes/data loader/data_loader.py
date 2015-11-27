abbec1678cb8572c0fea07d1a013213765e8b952
4r19415 <justin.bezieau@gmx.com>
2015-11-27 09:57:18 +0100

    code: data loader ok pour lire les donnees

--- codes/data loader/data_loader.py
+++ codes/data loader/data_loader.py
@@ -4,19 +4,19 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 
 def load_data(data_file):
-    
-    file=open(data_file,"r")
-    start=file.readline()
-    data=file.read()
-    print"start openning data : ",start,"\n",data
-    file.close()
-    dat_value=0
-    return(data) 
 
+    name = ["A", "time","B","x","y","z","C"]
+    pandas_data=pd.read_table(data_file, sep=";", skiprows=1, names=name, decimal=",")
+    print"\n", "pandas data",pandas_data
+
+    scatter(pandas_data["x"],pandas_data["y"])
+
+    return pandas_data
 
 def scatter(x,y):
     plt.scatter(x,y)
