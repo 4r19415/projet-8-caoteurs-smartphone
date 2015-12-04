003e5948f16ffbac1dcd355d8bf2e9483b6d3840
4r19415 <justin.bezieau@gmx.com>
2015-12-04 08:47:13 +0100

    codes, data loger, issus with the plot

--- codes/data loader/data_loader.py
+++ codes/data loader/data_loader.py
@@ -12,21 +12,21 @@ def load_data(data_file):
 
     name = ["A", "time","B","x","y","z","C"]
     pandas_data=pd.read_table(data_file, sep=";", skiprows=1, names=name, decimal=",")
-    print"\n", "pandas data",pandas_data
 
-    scatter(pandas_data["x"],pandas_data["y"])
+    scatter(pandas_data["x"],pandas_data["z"])
 
     return pandas_data
 
 def scatter(x,y):
-    plt.scatter(x,y)
+    print x,y
+    plt.scatter(np.array(x),np.array(y))
     plt.show()
 
 
 
 
 def main():
-   donee=load_data("LOG_2015-11-20_09-21-04mma8888_3-axis_Accelerometer.log")
+   donee=load_data("o.log")
 
 
 if __name__ == '__main__':
